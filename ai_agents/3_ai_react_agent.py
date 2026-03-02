472a5359c24910863b405228b430df6e26b37418
Yash Chauhan <yashc9411407582@gmail.com>
2026-03-02 13:14:37 +0530

    added code

--- ai_agents/3_ai_react_agent.py
+++ ai_agents/3_ai_react_agent.py
@@ -27,4 +27,87 @@ from langgraph.prebuilt import ToolNode
 # rule that controls how updates from nodes are combined with the existing state . 
 # tells us how to merge new data into the current state 
 
-# without a reducer , updates would have , replaced the existing value entirely  !
+# without a reducer , updates would have , replaced the existing value entirely
+
+class AgentState(TypedDict):
+    messages : Annotated[Sequence[BaseMessage],add_messages]
+
+
+
+@tool
+def add(a:int,b:int)->int : 
+    """This is an addition function that adds two numbers together """
+    return a+b
+
+@tool
+def multiply(a:int,b:int)->int : 
+    """This is an addition function that multiplies two numbers together """
+    return a*b
+
+@tool
+def subtract(a:int,b:int)->int : 
+    """This is an addition function that ads two numbers together """
+    return a-b
+
+tools = [add,multiply,subtract]
+
+model = ChatOllama(model='qwen2.5:3b-instruct-q4_0').bind_tools(tools)
+
+def model_call(state:AgentState)->AgentState:
+    system_prompt = SystemMessage(content='You are my ai assistant , please answer my query to the best of your ability')
+    response = model.invoke([system_prompt]+state['messages'])
+    return {"messages":[response]}
+
+
+def should_continue(state:AgentState)->AgentState:
+    """Decide wheather to continue or exit"""
+    messages = state['messages']
+    last_message = messages[-1]
+
+    if not last_message.tool_calls:
+
+        return "end"
+    else :
+        return "continue"
+
+
+graph = StateGraph(AgentState)
+
+graph.add_node("our_agent",model_call)
+
+# making tool node for the app
+
+tool_node = ToolNode(tools=tools)
+
+
+graph.add_node("tools",tool_node)
+
+
+graph.set_entry_point("our_agent")
+
+
+graph.add_conditional_edges(
+"our_agent",
+should_continue, # node  for routing 
+{
+    "continue":"tools",
+    "end":END
+}
+
+)
+
+graph.add_edge("tools","our_agent")
+
+app = graph.compile()
+
+
+def print_stream(stream):
+    for s in stream :
+        message = s['messages'][-1]
+        if isinstance(message,tuple):
+            print(message)
+        else : 
+            message.pretty_print()
+
+inputs = {"messages": [("user", "Add 40 + 12 and then multiply the result by 6. Also tell me a joke please.")]}
+print_stream(app.stream(inputs, stream_mode="values"))
